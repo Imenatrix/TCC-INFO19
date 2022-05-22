c036cad083902ddb99722cc84f7cdcafcb5ebc93
Ruan Gomes <ruangomesdorosario@gmail.com>
2022-05-21 21:25:35 -0300

    Implement DQN example

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
+from trainers.dqn_epsilon_keras import train
 from models.deepmind_atari import create_model
-import tensorflow as tf
-import keras
-import numpy as np
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
 
-model = create_model((28, 28, 1), 10)
+env = make_atari("BreakoutNoFrameskip-v4")
+env = wrap_deepmind(env, frame_stack=True, scale=True)
 
-# Prepare the training dataset.
-batch_size = 64
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-# x_train = np.reshape(x_train, (-1, 784))
-# x_test = np.reshape(x_test, (-1, 784))
+model = create_model(env.observation_space.shape, env.action_space.n)
+model_target = create_model(env.observation_space.shape, env.action_space.n)
 
-# Reserve 10,000 samples for validation.
-x_val = x_train[-10000:]
-y_val = y_train[-10000:]
-x_train = x_train[:-10000]
-y_train = y_train[:-10000]
-
-# Prepare the training dataset.
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-
-# Prepare the validation dataset.
-val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_dataset = val_dataset.batch(batch_size)
-
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
-
-model.fit(x_train, y_train, batch_size=batch_size, epochs=10, validation_split=0.2)
-
-test_scores = model.evaluate(x_test, y_test, verbose=2)
-print("Test loss:", test_scores[0])
-print("Test accuracy:", test_scores[1])
+train(model, model_target, env)
